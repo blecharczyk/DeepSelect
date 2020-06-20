3ee1964d05013a387bf258b932dc2c6224d32caa
krwozny <krwozny@student.agh.edu.pl>
2020-06-20 18:18:27 +0200

    Rename agent_type to behavior

--- deepselect/agent.py
+++ deepselect/agent.py
@@ -2,6 +2,6 @@ from deepselect.element import Element
 
 
 class Agent(Element):
-    def __init__(self, agent_id, data, resources, agent_type):
+    def __init__(self, agent_id, data, resources, behavior):
         Element.__init__(self, agent_id, data, resources)
-        self.agent_type = agent_type
+        self.behavior = behavior
