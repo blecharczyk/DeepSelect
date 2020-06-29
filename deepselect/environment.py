520fcda4adabf701481cfdd877980ea536596c99
krwozny <krwozny@student.agh.edu.pl>
2020-06-29 08:16:44 +0200

    Add nodes and edges to environment

--- deepselect/environment.py
+++ deepselect/environment.py
@@ -1,14 +1,35 @@
+import networkx as nx
 from random import shuffle
 
 from deepselect.category import Category
 from deepselect.categorizer import BehaviorCategorizer, UniformCategorizer
+from deepselect.node import Node
 
 
 class Environment:
     def __init__(self):
         self.nodes = []
+        self.graph = nx.Graph()
         self.agent_categorizer = BehaviorCategorizer()
         self.object_categorizer = UniformCategorizer(Category(name='Objects', color='black'))
+
+
+    def add_node(self, initial_resources):
+        node_id = len(self.nodes)
+        node = Node(node_id, initial_resources)
+
+        self.nodes.append(node)
+        self.graph.add_node(node_id, node=node)
+
+        return node
+
+
+    def add_edge(self, from_node, to_node):
+        if not self.graph.has_edge(from_node, to_node):
+            self.nodes[from_node].neighbors.append(to_node)
+            self.nodes[to_node].neighbors.append(from_node)
+            self.graph.add_edge(from_node, to_node)
+
 
     def step(self):
         nodes = self.nodes[:]
